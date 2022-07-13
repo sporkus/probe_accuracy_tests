07a172d88f48ce2d79693c8295139890cb831a45
spork <feedthefoon@gmail.com>
2022-07-13 08:26:56 -0700

    making installation more robust

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+REPO=probe_accuracy_tests
 
 cd "${HOME}" || exit
-echo "Cloning repo"
-git clone https://github.com/sporkus/probe_accuracy_tests
-cd "${HOME}"/probe_accuracy_tests || exit
-echo "Install python packages"
-python3 -c "import sys, pkgutil; sys.exit(0 if pkgutil.find_loader(sys.argv[1]) else 1)" pip || python3 -m pip install --upgrade
-pip3 install -r requirements.txt
-echo "Installation finished"
+# Clone repo if it doesn't exist
+[ -d "${REPO}" ] || git clone https://github.com/sporkus/probe_accuracy_tests 
+cd "${REPO}"; git pull
+
+# Install pip if it doesn't exist 
+[ -x "$(command -v pip3)" ] || sudo apt install python3-pip
+
+echo "Installing python packages"
+pip3 install -r requirements.txt --upgrade
 
 printf "\nUsage instructions:  python3 %s/probe_accuracy_tests/probe_accuracy_test_suite.py -h" "${HOME}"
 printf "\nMore details and updated instructions: \n\thttps://github.com/sporkus/probe_accuracy_tests/blob/master/README.md\n"
