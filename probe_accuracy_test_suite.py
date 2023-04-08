7fe7099ac2d776c479ff64e8f85008fa26632bd7
spork <feedthefoon@gmail.com>
2023-04-08 11:46:26 -0700

    add GPL

--- probe_accuracy_test_suite.py
+++ probe_accuracy_test_suite.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+# klipper probe accuracy test suite
+# Copyright (C) 2023 Foon Wong
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+# -----------------------------------------------------
 # Automating probe_accuracy testing
 # The following three tests will be done:
 # 0) 20 tests, 5 samples at bed center - check consistency within normal measurements
@@ -7,6 +26,7 @@
 # 2) 1 test, 30 samples at each bed mesh corners - check if there are issues with individual z drives
 # Notes:
 # * First probe measurements are dropped
+# -----------------------------------------------------
 
 import argparse
 import math
